b17281d94cf03bdd1a92ff1154d434add5f6c901
James Smith <james.smith@djalbat.com>
2016-03-05 15:51:00 +0000

    Simplified the grunt file.

--- Gruntfile.js
+++ Gruntfile.js
@@ -28,7 +28,7 @@ module.exports = function(grunt) {
       },
       npm: {
         command: [
-          'npm update easyui'
+          'npm install easyui@latest --save'
         ].join('&&')
       }
     },
@@ -57,42 +57,17 @@ module.exports = function(grunt) {
 
   grunt.registerTask('default', []);
 
-  grunt.registerTask('update', function(packageDirectory) {
-    var packageName = packageDirectory.toLowerCase(),
-        packageVersion = getPackageVersion(packageDirectory);
-
-    setDependenciesPackageVersion(packageName, packageVersion);
-  });
-
-  grunt.registerTask('u', ['update:EasyUI']);
-  grunt.registerTask('b', ['update:EasyUI', 'shell:npm', 'copy', 'browserify']);
-  grunt.registerTask('w', ['update:EasyUI', 'shell:npm', 'copy', 'browserify', 'watch']);
+  grunt.registerTask('b', ['shell:npm', 'copy', 'browserify']);
+  grunt.registerTask('w', ['shell:npm', 'copy', 'browserify', 'watch']);
   grunt.registerTask('g', function() {
     var bumpup_type = grunt.option('bumpup_type') || 'patch';
 
-    grunt.task.run('update:EasyUI');
     grunt.task.run('shell:npm');
     grunt.task.run('copy');
     grunt.task.run('browserify');
     grunt.task.run('bumpup:' + bumpup_type);
     grunt.task.run('shell:git')
   });
-
-  function getPackageVersion(packageDirectory) {
-    var packageFilePath = '../' + packageDirectory + '/package.json',
-        packageJSON = JSON.parse(fs.readFileSync(packageFilePath, 'utf8')),
-        packageVersionJSON = packageJSON["version"],
-        packageVersion = packageVersionJSON;  ///
-
-    return packageVersion;
-  }
-
-  function setDependenciesPackageVersion(packageName, packageVersion) {
-    var packageJSONFileName = 'package.json',
-        packageJSON = JSON.parse(fs.readFileSync(packageJSONFileName));
-
-    packageJSON["dependencies"][packageName] = "^" + packageVersion;
-
-    fs.writeFileSync(packageJSONFileName, JSON.stringify(packageJSON, '', '\t', 'utf8')); ///
-  }
 };
+
+
