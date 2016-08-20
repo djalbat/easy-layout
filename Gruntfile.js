cd843bf0e78e9694b71c27555b40a1dc5e8df344
James Smith <james.smith@djalbat.com>
2016-08-20 22:56:40 +0100

    Small changes to the grunt file.

--- Gruntfile.js
+++ Gruntfile.js
@@ -26,7 +26,7 @@ module.exports = function(grunt) {
           }
         },
         src: ['./index.js'],
-        dest: 'dist/easyui-layout.js'
+        dest: './dist/easyui-layout.js'
       }
     },
     bumpup: {
