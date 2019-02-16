48edd5b5edaa7625c5dcb512d97eee668ae54434
James Smith <james.smith@djalbat.com>
2019-02-16 14:15:08 +0000

    Removed the getters on the flexible and sizeable elements.

--- es6/flexible.js
+++ es6/flexible.js
@@ -5,22 +5,6 @@ const easy = require('easy');
 const { Element } = easy;
 
 class FlexibleElement extends Element {
-  getFlexibleElement() {
-    const flexibleElement = this; ///
-
-    return flexibleElement;
-  }
-
-  parentContext() {
-    const context = this.getContext(),
-          getFlexibleElement = this.getFlexibleElement.bind(this),
-          parentContext = Object.assign(context, {
-            getFlexibleElement
-          });
-
-    return parentContext;
-  }
-
   static fromProperties(properties) { return Element.fromProperties(FlexibleElement, properties); }
 }
 
--- es6/sizeable.js
+++ es6/sizeable.js
@@ -5,22 +5,6 @@ const easy = require('easy');
 const { Element } = easy;
 
 class SizeableElement extends Element {
-  getSizeableElement() {
-    const sizeableElement = this; ///
-
-    return sizeableElement;
-  }
-
-  parentContext() {
-    const context = this.getContext(),
-          getSizeableElement = this.getSizeableElement.bind(this),
-          parentContext = Object.assign(context, {
-            getSizeableElement
-          });
-
-    return parentContext;
-  }
-
   static fromProperties(properties) { return Element.fromProperties(SizeableElement, properties); }
 }
 
