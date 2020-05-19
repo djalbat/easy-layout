32fbb45359e79e217b38dd27f35d175d44f632d3
James Smith <james.smith@djalbat.com>
2020-05-19 17:17:27 +0100

    Removed the flexible div.

--- es6/index.js
+++ es6/index.js
@@ -7,7 +7,6 @@ export { default as RowsDiv } from "./div/rows";
 export { default as ColumnDiv } from "./div/column";
 export { default as ColumnsDiv } from "./div/columns";
 export { default as SplitterDiv } from "./div/splitter";
-export { default as FlexibleDiv } from "./div/flexible";
 export { default as SizeableDiv } from "./div/sizeable";
 export { default as VerticalSplitterDiv } from "./div/splitter/vertical";
 export { default as HorizontalSplitterDiv } from "./div/splitter/horizontal";
